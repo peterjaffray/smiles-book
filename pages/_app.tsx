f0cba5d0054b13f99ae20454c181e6cbaba77147
Pete <peter@choice.marketing>
2022-08-18 16:59:51 -0600

    Dental Clinic Application Forms

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+
+import type { AppProps } from 'next/app'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  
+  const emotionCache = clientSideEmotionCache    
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
+
+export default MyApp
